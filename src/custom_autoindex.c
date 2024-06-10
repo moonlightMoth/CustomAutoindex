9bd7a7895a248f749f21e4d52cdcf1cc23c44a57
moonlightMoth <jyhgftm@gmail.com>
2024-06-10 04:50:51 +0300

    asd

--- src/custom_autoindex.c
+++ src/custom_autoindex.c
@@ -3,11 +3,39 @@
 #endif
 
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <errno.h>
+
 #include "htmlprinter.h"
 
+#define HEDAER_FILE "header.html"
+#define FOOTER_FILE "footer.html"
 
 int main(int argc, char **args)
 {
+    long sz;
+    char* header_buffer;
+
+    FILE *fptr = fopen(HEDAER_FILE, "r");
+
+    if (fptr == NULL) {
+        perror("Cannot open html header file");
+        return 1;
+    }
+
+    header_buffer = get_file_content(fptr, &sz);
+
+    if (header_buffer == NULL)
+    {
+        perror("Cannot read html header file");
+        return 1;
+    }
+
+    while (!*header_buffer)
+    {
+        printf("%c", *header_buffer++);
+    }
     
-    printf("%d\n", test(12));
+    
+    
 }
--- src/htmlprinter.h
+++ src/htmlprinter.h
@@ -3,3 +3,37 @@ int test(int a)
 {
 	return 2 * a;
 }
+
+char* get_file_content(FILE* fptr, long* returnSize)
+{
+	int i;
+	char* retPtr;
+
+	if (fptr == NULL || returnSize == NULL)
+	{
+		perror("NULL in get_header");
+		return NULL;
+	}
+
+	if (fseek(fptr, 0, SEEK_END) < 0)
+	{
+		perror("Got zero size get_file_content");
+		return NULL;
+	}
+	
+	*returnSize = ftell(fptr);
+	retPtr = (char*) calloc(1, (*returnSize + 1) * sizeof(char));
+	if (retPtr == NULL)
+	{
+		perror("NULL in get_header");
+		return NULL;
+	}
+	
+
+	fgets(retPtr, *returnSize, fptr);
+	fclose(fptr);
+
+	retPtr[*returnSize+1] = 0;
+	
+	return retPtr;
+}
