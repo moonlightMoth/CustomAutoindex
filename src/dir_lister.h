37f1c197da9f0d74b0b82b569084bb2ba58ef90b
moonlightmoth <ghjpa@inbox.ru>
2024-07-01 12:37:49 +0000

    refactor

--- src/dir_lister.h
+++ src/dir_lister.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 
 #define MAX_NAME_LEN 51
@@ -9,12 +12,59 @@ typedef struct
 	char** children;
 } dir_tree;
 
-int get_dir_len()
+int __get_dir_len()
 {
 	
 }
 
-int list_dirs(char **buff, int buff_length)
+dir_tree* list_dirs(char* root_name)
 {
+	dir_tree *root = (dir_tree*) malloc(sizeof(dir_tree));
+
+	int num = 0, i, j, root_len = strlen(root_name);
+	DIR *dp;
+	struct dirent *ep;
+
+	dp = opendir(root_name);
+	if (dp != NULL)
+	{
+		while (ep = readdir(dp))
+			num++;
+		closedir(dp);
+	}
 	
+
+	root->num_of_children = num;
+	root->children = malloc(num*sizeof(char*));
+	root->name = malloc(root_len);
+	memcpy(root->name, root_name, root_len);
+
+	for (i = 0; i < num; i++)
+	{
+		root->children[i] = malloc(MAX_NAME_LEN);
+	}
+
+	i = 0; j = 0;
+
+	dp = opendir(root_name);
+	if (dp != NULL)
+	{
+		while (ep = readdir(dp))
+		{
+			j = 0;
+
+			while (ep->d_name[j])
+			{
+				root->children[i][j] = ep->d_name[j];
+				j++;
+			}
+			root->children[i][j] = '\0';
+
+			i++;
+		}
+		closedir(dp);
+	}
+
+	return root;
 }
+
--- src/dir_lister_test.c
+++ src/dir_lister_test.c
@@ -8,52 +8,9 @@
 
 int main()
 {
-	dir_tree *root = (dir_tree*) calloc(1, sizeof(dir_tree));
+	dir_tree *root = list_dirs("test");
 
-	int num = 0, i, j;
-	DIR *dp;
-	struct dirent *ep;
-
-	dp = opendir("./test");
-	if (dp != NULL)
-	{
-		while (ep = readdir(dp))
-			num++;
-		closedir(dp);
-	}
-
-	root->num_of_children = num;
-	root->children = malloc(num*sizeof(char*));
-	root->name = malloc(7);
-	memcpy(root->name, "test/0", 5);
-
-	for (i = 0; i < num; i++)
-	{
-		root->children[i] = malloc(MAX_NAME_LEN);
-	}
-
-	i = 0; j = 0;
-
-	dp = opendir("./test");
-	if (dp != NULL)
-	{
-		while (ep = readdir(dp))
-		{
-			j = 0;
-
-			while (ep->d_name[j])
-			{
-				root->children[i][j] = ep->d_name[j];
-				j++;
-			}
-			root->children[i][j] = '\0';
-
-			i++;
-		}
-		closedir(dp);
-	}
-
-	for (i = 0; i < root->num_of_children; i++)
+	for (int i = 0; i < root->num_of_children; i++)
 	{
 		printf("%s\n", root->children[i]);
 	}
