40dde987507a668e464d3339577e7e3d17527b1b
moonlightmoth <ghjpa@inbox.ru>
2024-07-01 11:20:12 +0000

    dir_started

---
+++ src/dir_lister.h
@@ -0,0 +1,20 @@
+#include <dirent.h>
+
+#define MAX_NAME_LEN 51
+
+typedef struct
+{
+	char* name;
+	int num_of_children;
+	char** children;
+} dir_tree;
+
+int get_dir_len()
+{
+	
+}
+
+int list_dirs(char **buff, int buff_length)
+{
+	
+}
