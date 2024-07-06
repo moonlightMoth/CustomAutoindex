b96d8931348cb58cebb1c1de1f635849d2e77be1
moonlightmoth <ghjpa@inbox.ru>
2024-07-06 09:09:59 +0300

    refactor

---
+++ .test/chudo/src/main/java/yandex/cloud/examples/serverless/todo/db/Dao.java
@@ -0,0 +1,15 @@
+package yandex.cloud.examples.serverless.todo.db;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    List<T> findAll();
+
+    void save(T t);
+
+    void deleteById(String id);
+
+    void UpdateById(String taskId, String description); 
+
+}
