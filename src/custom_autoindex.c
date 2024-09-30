6917d95182dfde26aa8e1a03ced1eb133beda548
moonlightMoth <jyhgftm@gmail.com>
2024-10-01 00:27:57 +0300

    asd

--- src/custom_autoindex.c
+++ src/custom_autoindex.c
@@ -7,10 +7,9 @@
 					"custom_autoindex <opt> <directory>\n" \
 					"   <directory>   root of directory tree to generate html from\n" \
 					"Opt:\n" \
-					"   -t, --tree     make tree with full depth starting from <directory> and exit\n" \
-					"   , result will be printed to tree.html\n" \
+					"   -t, --tree     make tree with full depth starting from <directory> to stdout and exit\n" \
 					"   -s, --serve    start http server on port 8080 to generate html from <directory> root\n" \
-					"   -S, --single   generate single html that contains <directory> content\n"
+					"   -S, --single   print to stdout single html that contains <directory> content\n"
 
 int main (int argc, char** argv)
 {
